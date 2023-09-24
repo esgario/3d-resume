9784f26c97b73862fb5f74832f0042a91499551a
esgario <guilherme.esgario@gmail.com>
2023-09-24 19:28:29 -0300

    update generate_lipsync

--- utils/commons.py
+++ utils/commons.py
@@ -1,6 +1,9 @@
 import json
+import logging
+
 
 def get_audios(reference_path):
+    """Get audios from reference file"""
     with open(reference_path, "r") as file:
         audios_reference = json.load(file)
 
@@ -8,3 +11,17 @@ def get_audios(reference_path):
         for lang, texts in texts_by_language.items():
             for i, text in enumerate(texts):
                 yield audio_name, lang, i, text
+
+
+def get_logger(name):
+    """Create a new logger"""
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
--- utils/generate_lipsync.py
+++ utils/generate_lipsync.py
@@ -3,8 +3,9 @@ import os
 import json
 import subprocess
 from contextlib import contextmanager
-from commons import get_audios
+from commons import get_audios, get_logger
 
+logger = get_logger(__name__)
 
 RHUBARB_VERSION = "1.13.0"
 RHUBARB_FILE = f"Rhubarb-Lip-Sync-{RHUBARB_VERSION}-Linux"
@@ -25,7 +26,7 @@ def workdir():
 def download_rhubarb():
     """Download rhubarb and unzip it"""
     if os.path.exists(RHUBARB_FILE):
-        print(f"{RHUBARB_FILE} already exists, skipping download")
+        logger.info("%s already exists, skipping download" % RHUBARB_FILE)
         return
 
     base_url = "https://github.com/DanielSWolf/rhubarb-lip-sync"
@@ -38,13 +39,16 @@ def download_rhubarb():
 def run_rhubarb():
     """Generate lipsync data using rhubarb"""
 
-    for audio_name, lang, i, _ in get_audios(REFERENCE_PATH):
+    for audio_name, lang, i, text in get_audios(REFERENCE_PATH):
         audio_path = f"{AUDIOS_PATH}/{audio_name}_{lang}-{i}.ogg"
         json_path = f"{AUDIOS_PATH}/{audio_name}_{lang}-{i}.json"
 
         if os.path.exists(json_path):
-            print(f"{json_path} already exists, skipping")
-            continue
+            with open(json_path, "r", encoding="utf-8") as file:
+                data = json.load(file)
+            if data["metadata"].get("text", "") == text:
+                logger.info("%s already exists, skipping" % json_path)
+                continue
 
         rhubarb_command = [
             f"{RHUBARB_FILE}/rhubarb",
@@ -55,15 +59,22 @@ def run_rhubarb():
             json_path,
         ]
 
-        print("Generating lipsync data for", audio_path, "to", json_path)
+        logger.info("Generating lipsync data for %s to %s" % (audio_path, json_path))
 
         rhubarb_process = subprocess.Popen(
             rhubarb_command,
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
         )
-
         rhubarb_process.wait()
+
+        with open(json_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            data["metadata"]["text"] = text
+            data["metadata"]["soundFile"] = json_path
+
+        with open(json_path, "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4)
 
 
 with workdir():
