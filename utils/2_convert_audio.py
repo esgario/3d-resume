a56d2cdfc0ce43c246559ff2e9a7ec65dacb6e21
esgario <guilherme.esgario@gmail.com>
2023-10-02 09:11:51 -0300

    update answers

--- utils/2_convert_audio.py
+++ utils/2_convert_audio.py
@@ -7,11 +7,13 @@ with workdir():
         for i in range(len(answers)):
             src_path = make_audio_path(lang, key, i, "mp3")
             dst_path = make_audio_path(lang, key, i, "ogg")
-            print("Converting", src_path, "to", dst_path)
 
-            # Convert mp3 to ogg
-            sound = AudioSegment.from_file(src_path, format="mp3")
-            sound.export(dst_path, format="ogg")
+            if os.path.exists(src_path):
+                print("Converting", src_path, "to", dst_path)
 
-            # Remove mp3 file
-            os.remove(src_path)
+                # Convert mp3 to ogg
+                sound = AudioSegment.from_file(src_path, format="mp3")
+                sound.export(dst_path, format="ogg")
+
+                # Remove mp3 file
+                os.remove(src_path)
